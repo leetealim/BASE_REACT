6d8e4ef0e78165dd3557cfd516d981a809698eee
leetealim <goodlooking55@daum.net>
2022-06-20 10:39:02 +0900

    playWrap

--- src/components/5_MusicApp/components/PlayWrap.js
+++ src/components/5_MusicApp/components/PlayWrap.js
@@ -1,8 +1,27 @@
+import styledComponents from "styled-components";
 import styled from "styled-components";
 
-const SPlayWrap = styled.div``;
-const GageWrap = styled.div``;
-const Gage = styled.div``;
+const SPlayWrap = styled.div`
+  margin-top: 30px;
+  margin-top: 30px;
+`;
+const GageWrap = styled.div`
+  width: 100%;
+  height: 5px;
+  background-color: gray;
+`;
+const Gage = styled.div`
+  width: 10%;
+  height: 100%;
+  background-color: mediumpurple;
+`;
+const spen = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-top: 5px;
+  font-size: 12px;
+  font-weight: 100;
+`;
 
 export const PlayWrap = () => {
   return (
@@ -10,8 +29,10 @@ export const PlayWrap = () => {
       <GageWrap>
         <Gage />
       </GageWrap>
-      <spen>00:03</spen>
-      <spen>03:40</spen>
+      <Timewrap>
+        <spen>00:03</spen>
+        <spen>03:40</spen>
+      </Timewrap>
     </SPlayWrap>
   );
 };
